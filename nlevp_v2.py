79fcd17320634ec965217c503d15e17f53c49fe2
Issa <70990469+ikarambal@users.noreply.github.com>
2020-09-30 15:03:01 -0400

    Update nlevp_v2.py

--- nlevp_v2.py
+++ nlevp_v2.py
@@ -23,7 +23,8 @@ class nnlinear_holom_eigs_solver(object):
     """
     def __init__(self, m, N, l, K, mat_func, cont_func, Dcont_func, cont_func_params, rankTol=1e-4, resTol=1e-6):
         """
-        if the derivative is not provided, thinking to implement a second order method to get the Jacobian (finite difference)
+        If the derivative is not provided, thinking to implement a second order method to get the Jacobian (finite difference)
+        Need to make it for more general closed contour
         """
         self.m = m
         self.l = l
@@ -60,10 +61,9 @@ class nnlinear_holom_eigs_solver(object):
         """
         Implement trapezoid to compute the integrals
         """
-        #Maybe have the user to input his/her own points, [start, end, step=(start-end)/N]
         #t = np.linspace(start, end, self.N, endpoint=False)
         t_points = np.linspace(0, 2*np.pi, self.N, endpoint=False)
-        #Deos the choice fo the distribution influence the process?
+        #Deos the choice of the distribution influences the process?
         Vhat = np.random.randn(self.m, l)
         blck_mat_lst = []
         for p in range(2*self.K):
@@ -77,47 +77,47 @@ class nnlinear_holom_eigs_solver(object):
         B0 = self.BlockMatrix(blck_mat_lst[: -1])
         B1 = self.BlockMatrix(blck_mat_lst[1: ])
         
-        V, SIGMA, Wh = svd(B0, full_matrices=False)
+        V, sigma, Wh = svd(B0, full_matrices=False)
         W            = inv( Wh )
-        SIGMA0       = SIGMA[np.abs(SIGMA) > self.rankTol]
+        sigma0       = sigma[np.abs(sigma) > self.rankTol]
         #k is always less or equal to l<=m is the number of eigenvalues inside the contour
         #if k=len(SIGMA0)=l then there may be more than l eigenvalues inside the contour
         #eigs close to the contour either inside or outside may lead to difficulties in the rank test
-        return [SIGMA0, V, W, B1]
+        return [sigma0, V, W, B1]
 
     def eigvals(self):
         """
         compute eigvalues and eigenvectors where l is adaptively computed.
         
         """
-        LAMBDA = []
-        VECTORS = []
+        eigenvals = []
+        eigenvecs = []
         c = self.cont_func_params['center']
         R = self.cont_func_params['radius']
         l = self.l
         while True:
-            SIGMA0, V, W, A_1N =  self.ContourIntegralEval(l)
-            k = len(SIGMA0)
+            sigma0, V, W, A_1N =  self.ContourIntegralEval(l)
+            k = len(sigma0)
             if k < l*self.K:
                 #the inverse of sigma0 might not exists also there might be cases where k=0...need to solve them
-                SIGMA0_INV = np.diag( 1/SIGMA0 )#the inverse
+                sigma0_inv = np.diag( 1/sigma0 )#the inverse
                 V0         = V[:self.K*self.m, :k]
                 W0         = W[:self.K*l, :k]
-                B          = np.dot( np.dot( V0.conj().T, A_1N), np.dot( W0, SIGMA0_INV ) )
+                B          = np.dot( np.dot( V0.conj().T, A_1N), np.dot( W0, sigma0_inv ) )
                 EIGVALS, EIGVECTS = eig( B )
                 for pos, la_i in enumerate( EIGVALS ):
-                    eigvect_i = np.dot( V0, EIGVECTS[:, pos] )
-                    if norm( np.dot( self.mat_func(la_i), eigvect_i ), np.inf) <= self.resTol and np.abs(la_i - c) < R:
-                        VECTORS.append(eigvect_i)
-                        LAMBDA.append(la_i)
+                    v_i = np.dot( V0[:m, :], EIGVECTS[:, pos] )
+                    if norm( np.dot( self.mat_func(s_i), v_i ), np.inf) <= self.resTol and np.abs(s_i - c) < R:
+                        eigenvecs.append(v_i)
+                        eigenvals.append(s_i)
                 break
                 
             else: 
                 l = l + 1
         #Neet to think of an efficient and more elegant way
-        if len(LAMBDA) == 0:
+        if len(eigenvals) == 0:
             print( 'No eigenvalues were found inside/outside the given countour!')
             pass
         else:
-            return (np.array( LAMBDA), np.array(VECTORS))
+            return (np.array(eigenvals), np.array(eigenvecs))
    
